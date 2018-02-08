faa34a41e35283b7def1765e32219d58c750c720
John Krull <john.a.krull@gmail.com>
2018-02-08 16:08:18 -0600

    Allow gab to work regardless of username

--- fish/functions/gab.fish
+++ fish/functions/gab.fish
@@ -1,6 +1,6 @@
 function gab --description 'Show current branch of selected dev directories'
 	for path in $GITDIRS
-set branch (git --git-dir=/home/krullj/dev/$path/.git --work-tree=/home/krullj/dev/$path rev-parse --abbrev-ref HEAD)
-printf "%10s %s\n" $path $branch
-end
+        set branch (git --git-dir=/home/$USER/dev/$path/.git --work-tree=/home/$USER/dev/$path rev-parse --abbrev-ref HEAD)
+        printf "%10s %s\n" $path $branch
+    end
 end
